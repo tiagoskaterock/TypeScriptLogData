44c0c264689147673a1c91088abd7545d848bbc0
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-07-09 06:12:40 -0300

    77. A Subtle Adjustment

--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
 import axios from "axios"
 
-const url = 'https://jsonplaceholder.typicode.com/posts/1';
+const url = 'https://jsonplaceholder.typicode.com/posts/1'
 
 axios.get(url).then(res=>{
-	console.log(res.data);
+	const post = res.data
+	const id = post.userId
+	const title = post.title
+	const body = post.body
+	console.log(`
+User ID: ${id}
+Title: ${title}
+Content: ${body}
+	`)
 })
+
