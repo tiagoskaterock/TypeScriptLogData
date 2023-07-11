b60dcea5dd568cc0648302fa0a6ab385c5887484
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-07-11 02:59:38 -0300

    78. Catching Errors in Typescript

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import axios from "axios"
 
+interface Post {
+	userId: number;
+	title: string;
+	body: string;
+}
+
 const url = 'https://jsonplaceholder.typicode.com/posts/1'
 
 axios.get(url).then(res=>{
-	const post = res.data
+	const post = res.data as Post
 	const id = post.userId
 	const title = post.title
 	const body = post.body
@@ -14,3 +20,11 @@ Content: ${body}
 	`)
 })
 
+/*
+{
+  "userId": 1,
+  "id": 1,
+  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+}
+*/
